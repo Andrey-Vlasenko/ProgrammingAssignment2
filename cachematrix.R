98fbb184ae795655f0a42d9deb7325071a117e10
Andrey-Vlasenko <andrey05@yandex.ru>
2015-12-17 16:25:28 +0300

    cachematrix.R update

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Below are two functions that are used to create a special
+# object that stores a numeric matrix and cache's its invertion
 
-## This function creates a special "matrix" object that can cache its inverse
-
+## This function creates a special "matrix" object 
+## that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
 	m <- NULL
 	set <- function(y) {
 		x <<- y
 		m <<- NULL
 	}
-	get <- function() x
-	setInverse <- function(InverseM) m <<- InverseM
-	getInverse <- function() m
+	get <- function() x 
+	setsolve <- function(solve) m <<- solve
+	getsolve <- function() m
 	list(set = set, get = get,
-		setInverse = setInverse,
-		getInverse = getInverse)
+		setsolve = setsolve,
+		getsolve = getsolve)
 }
 
-## This function computes the inverse of the special "matrix" returned by 
-## makeCacheMatrix above. If the inverse has already been calculated (and the matrix
-## has not changed), then the cachesolve should retrieve the inverse from the cache.
-
+## This function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. If the inverse has 
+## already been calculated (and the matrix has not changed),
+## then the cachesolve should retrieve the inverse  
+## from the cache.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 
-	if (det(x) != 0) { ### Caurse we can assume that the matrix is invetible, but  
-				 ### it's rather easy to check (if we aren't talking about the 
-				 ### pseudoinversion of matrix, which could be applied to any matrix).
+	if (det(x$get()) != 0) { 
+	### Course we can assume that the matrix is invetible,
+	### but it's rather easy to check (if we aren't talking 
+	### about the pseudoinversion of matrix).
 
-		m <- x$getInverse()
+		m <- x$getsolve()
 		if(!is.null(m)) {
 			message("getting cached data")
 			return(m)
@@ -36,11 +38,12 @@ cacheSolve <- function(x, ...) {
 
 		data <- x$get()
 		m <- solve(data, ...)
-		x$setInverse(m)
+		x$setsolve(m)
 		m
 	}
 	else { 
 		message("Matrix is not invertible!") 
 		return(m)
+		### Probably we could do something else
 	}
 }
